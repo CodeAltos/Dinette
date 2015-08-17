7ea164a6bf5e699904c0f8956ec60fb91ecf4fc6
Jenny Qian <jqian@tendenci.com>
2015-08-17 10:06:10 -0500

    fix for search_indexes

--- dinette/search_indexes.py
+++ dinette/search_indexes.py
@@ -1,16 +1,23 @@
 from haystack import indexes
-from haystack import site
 
 from dinette.models import Ftopics, Reply, DinetteUserProfile
 
-class TopicIndex(indexes.SearchIndex):
+class TopicIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     subject = indexes.CharField(model_attr="subject")
     message = indexes.CharField(model_attr="_message_rendered")
     
+    @classmethod
+    def get_model(self):
+        return Ftopics
+    
 class ReplyIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     message = indexes.CharField(model_attr="_message_rendered")
+    
+    @classmethod
+    def get_model(self):
+        return Reply
     
 class UserprofileIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
@@ -18,6 +25,7 @@ class UserprofileIndex(indexes.SearchIndex):
     first_name = indexes.CharField(model_attr="first_name")
     last_name = indexes.CharField(model_attr="last_name")
     
-site.register(Ftopics, TopicIndex)
-site.register(Reply, ReplyIndex)
-site.register(DinetteUserProfile, UserprofileIndex)
+    @classmethod
+    def get_model(self):
+        return DinetteUserProfile
+
